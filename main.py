db7a5f3434f26bdd24f01867307d52113c59e295
Nathan Krasney <natankrasney@gmail.com>
2021-01-26 12:14:38 +0200

    documentation and more arguments

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import plot_with_residual
 
-X = np.array([1,2,3])
-Y = np.array([2,1,5])
+X = np.array([1,5,6])
+Y = np.array([4,9,13])
 
-plot_with_residual(X,Y,0,1)
-plt.title("residual plot")
+plot_with_residual(plt,X,Y,2,1.5)
+plt.title("data set vs linear line + residual")
 plt.show()
--- utils.py
+++ utils.py
@@ -2,14 +2,37 @@ import matplotlib.pyplot as plt
 
 
 def linear_line(x,intercept,slope):
+    """[summary]
+    compute the output of a linear line
+
+    Args:
+        x ([type]): [description]
+        intercept (number): [description]
+        slope (number): [description]
+
+    Returns:
+        [type]: [description]
+    """
     return slope * x + intercept
 
 
-def plot_with_residual(X,Y,intercept,slope,color='g',linewidth=3):
+def plot_with_residual(target_plt,X,Y,intercept,slope,color='g',linewidth=3):
+    """[summary]
+    plot data set X,Y vs linear line and residual i.e. the distance between actual - Y[i] and estimated - h[i]
+    
+    Args:
+        target_plt () : e.g. plt or axs (plt.subplots)
+        X ([number]): vector input
+        Y ([number]): vector output
+        intercept (number): value of linear line @ x=0
+        slope (number): slope of linear line
+        color (str, optional): color of residual line. Defaults to 'g'.
+        linewidth (int, optional): width of residual line. Defaults to 3.
+    """
     h = linear_line(X,intercept,slope)
-    plt.plot(X,Y,'o',X,h)
+    target_plt.plot(X,Y,'o',X,h)
     m = X.size
     i = 0
     while i < m:
-        plt.plot([X[i],h[i]],[X[i],Y[i]],color=color,linewidth=linewidth)
+        target_plt.plot([X[i],X[i]],[h[i],Y[i]],color=color,linewidth=linewidth)
         i += 1
